ffeced0041b4c548355b0f8f6b1012114901f50b
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-16 20:38:43 +0800

    tried allwing all origins inside cors

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['*'],
     'allowed_headers' => ['Content-Type', 'Authorization'],
     'exposed_headers' => [],
     'max_age' => 0,
